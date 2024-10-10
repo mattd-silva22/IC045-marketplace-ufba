746ef6ea0548e53ba8628bce8c95dfe165046e4b
matheus.barretto <matheus.barretto2001@gmail.com>
2024-10-09 23:10:11 -0300

    feat(backend) Add prisma orm ao projeto e fiz configuração inicial do banco em docker

---
+++ apps/api/prisma/seed.ts
@@ -0,0 +1,39 @@
+import { PrismaClient } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+const prisma = new PrismaClient();
+
+async function main() {
+  // Criar usuários de exemplo
+  const user1 = await prisma.tbUsuario.create({
+    data: {
+      nome: 'Alice Silva',
+      img: 'https://example.com/alice.jpg',
+      email: 'alice@example.com',
+      senha: await bcrypt.hash('123456', 10), // Em produção, não armazene senhas em texto plano
+      id_papel: 1, // Ajuste conforme necessário
+    },
+  });
+
+  const user2 = await prisma.tbUsuario.create({
+    data: {
+      nome: 'João Souza',
+      img: 'https://example.com/joao.jpg',
+      email: 'joao@example.com',
+      senha: await bcrypt.hash('123456', 10), // Em produção, não armazene senhas em texto plano
+      id_papel: 2, // Ajuste conforme necessário
+    },
+  });
+
+  console.log({ user1, user2 });
+  console.log('Banco de dados preenchido com dados padrão.');
+}
+
+main()
+  .catch((e) => {
+    console.log('Falha ao preencher Banco de dados.');
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
