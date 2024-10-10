6bb80c4668d5b09bcf91f972869e694fd8963b27
Pedro Jefferson <pedrojefferson.dev@gmail.com>
2024-10-10 14:53:04 -0300

    Add tests to auth.controller.ts

--- apps/api/src/auth/auth.service.ts
+++ apps/api/src/auth/auth.service.ts
@@ -37,6 +37,7 @@ export class AuthService {
 
   async login(user: any) {
     const payload = { username: user.username, sub: user.id };
+
     return {
       access_token: this.jwtService.sign(payload),
     };
--- apps/api/src/auth/jwt.strategy.ts
+++ apps/api/src/auth/jwt.strategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'yourSecretKey',
+      secretOrKey: process.env.JWT_KEY,
     });
   }
 
